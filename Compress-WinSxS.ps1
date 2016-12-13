69aad4bd2e7ee7ea9b82d8d90c1a4da8795baf68
Jaap Brasser <noreply@jaapbrasser.com>
2016-12-13 11:46:54 +0100

    Implemented wmi support for backwards compatibility

--- Compress-WinSxS.ps1
+++ Compress-WinSxS.ps1
@@ -1,23 +1,53 @@
-﻿$Service = @{}
-$Service.MSIServer, $Service.TrustedInstaller = Get-Service -Name msiserver,trustedinstaller
+﻿function Test-ServiceObject {
+    $ServiceObject = New-Object -TypeName System.ServiceProcess.ServiceController
+    if (Get-Member -InputObject $ServiceObject -MemberType Property -Name StartType) {
+        $true
+    } else {
+        $false
+    }
+}
 
-# Stop services, set start up to Disabled
+# Set startup mode to Disabled and store current startup configuration
+$Service = @{}
+if (Test-ServiceObject) {
+    $Service.MSIServer, $Service.TrustedInstaller = Get-Service -Name msiserver,trustedinstaller |
+                                                    Select-Object -ExpandProperty StartType
+    Get-Service -Name msiserver,trustedinstaller | Set-Service -StartupType Disabled -Verbose
+} else {
+    $Service.MSIServer        = Get-WmiObject -Query "Select StartMode FROM win32_service Where name='msiserver'" |
+                                Select-Object -ExpandProperty StartMode
+    $Service.TrustedInstaller = Get-WmiObject -Query "Select StartMode FROM win32_service Where name='trustedinstaller'" |
+                                Select-Object -ExpandProperty StartMode
+}
+
+# Stop services
 Get-Service -Name msiserver,trustedinstaller | Stop-Service -Force -Verbose
-Get-Service -Name msiserver,trustedinstaller | Set-Service -StartupType Disabled -Verbose
 
 # Backup WinSxS ACL
-C:\WINDOWS\system32\icacls.exe "$env:windir\WinSxS" /save "$env:USERPROFILE\Backupacl.acl"
+& ${env:windir}\system32\icacls.exe "${env:windir}\WinSxS" /save "${env:userprofile}\Backupacl.acl"
 
-C:\WINDOWS\system32\takeown.exe /f "$env:windir\WinSxS" /r
-C:\WINDOWS\system32\icacls.exe "$env:windir\WinSxS" /grant "$env:USERDOMAIN\$env:USERNAME":(F) /t
-C:\WINDOWS\system32\compact.exe /s:"$env:windir\WinSxS" /c /a /i *
-C:\WINDOWS\system32\icacls.exe "$env:windir\WinSxS" /setowner "NT SERVICE\TrustedInstaller" /t
+# Take ownership and set ACL
+& ${env:windir}\system32\takeown.exe /f "${env:windir}\WinSxS" /r
+& ${env:windir}\system32\icacls.exe "${env:windir}\WinSxS" /grant "${env:userdomain}\${env:username}:(F)" /t
+
+# Compress WinSxS
+& ${env:windir}\system32\compact.exe /s:"${env:windir}\WinSxS" /c /a /i *
 
 # Restore WinSxS ACL
-C:\WINDOWS\system32\icacls.exe "$env:windir\WinSxS" /restore "$env:USERPROFILE\Backupacl.acl"
-# Remove-Item "$env:USERPROFILE\Backupacl.acl"
+& ${env:windir}\system32\icacls.exe "${env:windir}\WinSxS" /setowner "NT SERVICE\TrustedInstaller" /t
+& ${env:windir}\system32\icacls.exe "${env:windir}\WinSxS" /restore "${env:userprofile}\Backupacl.acl"
+
+# Remove backup acl
+Remove-Item "${env:userprofile}\Backupacl.acl"
 
 # Start services and set startup to old value
-Set-Service -Name msiserver -StartupType $Service.MSIServer.StartType
-Set-Service -Name trustedinstaller -StartupType $Service.TrustedInstaller.StartType
+if (Test-ServiceObject) {
+    Set-Service -Name msiserver -StartupType $Service.MSIServer
+    Set-Service -Name trustedinstaller -StartupType $Service.TrustedInstaller
+} else {
+    (Get-WmiObject -Query "Select * FROM win32_service Where name='msiserver'").ChangeStartMode($Service.MSIServer)
+    (Get-WmiObject -Query "Select * FROM win32_service Where name='trustedinstaller'").ChangeStartMode($Service.TrustedInstaller)
+}
+
+# Start services
 Start-Service -Name msiserver,trustedinstaller
