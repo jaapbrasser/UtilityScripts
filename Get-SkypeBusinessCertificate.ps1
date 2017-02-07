88d211b22c8e75b10bddcc6d395f8f3a91b2d3aa
Jaap Brasser <noreply@jaapbrasser.com>
2017-02-07 16:23:29 +0100

    Added Get-SkypeBusinessCert script

---
+++ Get-SkypeBusinessCertificate.ps1
@@ -0,0 +1,15 @@
+﻿function Get-SkypeBusinessCertificate {
+    [System.IO.File]::WriteAllBytes("$env:USERPROFILE\desktop\EventCert.cer",
+        (
+            (
+                [xml](Get-WinEvent -FilterHashtable @{
+                    'Logname' = 'System'
+                    'Id'      = 36882
+                } -MaxEvents 1).ToXml()
+            ).Event.Eventdata.Binary -split '(..)' |
+            Where-Object {$_} | ForEach-Object {
+                [system.convert]::ToByte($_,16)
+            }
+        )
+    )
+}
