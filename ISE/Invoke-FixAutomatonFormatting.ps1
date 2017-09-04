a5228ce99ed25c04bab88b9018ebd35fc603470b
Jaap Brasser <noreply@jaapbrasser.com>
2017-09-04 15:07:53 +0200

    Changed to single quotes

--- ISE/Invoke-FixAutomatonFormatting.ps1
+++ ISE/Invoke-FixAutomatonFormatting.ps1
@@ -1,21 +1,22 @@
 ﻿$null = $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(
     'Invoke-FixAutomatonFormatting', {
-$psISE.CurrentFile.Editor.Text = @'
+$psISE.CurrentFile.Editor.Text = @"
 `$moduleDir = "`${Reporting_Module_Path}"
 `$script = 'Module-AllAutomations.psm1'
 `$ps = Join-Path -Path `$moduleDir -ChildPath `$script
 
-`$Output = `@"
+`$Output = `@'
 $($psISE.CurrentFile.Editor.SelectedText -replace "(Write-Output -InputObject ')|((\;)*'\|\ Out-File\ \`$ps)|( -Append)"  -replace '(?smi)(^\s+)',"    ")
-"`@
+'`@
 
 Set-Content -Path `$PS -Value `$Output
-'@
+"@
     }, 'CTRL+ALT+I'
 )
 
 <# Remove the script
 
 $remove = $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Remove($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Where{$_.DisplayName -eq 'Invoke-FixAutomatonFormatting'})
+$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Remove($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus[1])
 
-#>$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Remove($psISE.CurrentPowerShellTab.AddOnsMenu.Submenus[1])
+#>
